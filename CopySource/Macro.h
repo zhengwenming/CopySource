3c60b2ab73e43ae766f033f1379cbe6cacbcf560
zhengwenming <740747055@qq.com>
2017-08-19 15:57:33 +0800

    delete something

--- CopySource/Macro.h
+++ CopySource/Macro.h
@@ -39,57 +39,8 @@
 
 
 #define TheUserDefaults [NSUserDefaults standardUserDefaults]
-/// NSUserDefaults 存储的投保人信息
-#define kApplicantPerson        [UserInfo sharedInfo].customerId
-/// NSUserDefaults 存储的被保人信息
-#define kInsuredPerson          @"insuredPerson"
-/// NSUserDefaults 存储的产品信息字典
-#define kProductDic          @"kProductDic"
-/// NSUserDefaults 存储的推送消息数组
-#define kMessageDataDic          @"kMessageDataDic"
-
-/// 本地缓存图片的文件夹
-#define kCacheUploadFiles         @"uploadFiles"
 
 
-/// 产品详情URL
-#define kProductDetailURL           @"http://www.aijkang.com/app/index.html"
-/// 产品详情分享URL
-#define kShareProductDetailURL           @"http://www.aijkang.com/wechat/index.html"
-
-
-/// 邀请/分享URL
-#define kInviteAcceptURL            @"http://www.aijkang.com/app/invite.html"
-//#define kInviteAcceptURL            @"http://10.0.2.82/app/inviteTest.html"
-
-/// 健康告知URL
-#define kHealthInfoURL              @"http://www.aijkang.com/app/health.html"
-
-/// 投保须知
-#define kInsuranceInformationURL    @"http://www.aijkang.com/app/needTo.html"
-
-///保险条款
-#define kInsuranceClauseURL    @"http://www.aijkang.com/app/clause.html"
-
-///理赔须知
-#define kClaimsURL  @"http://www.aijkang.com/app/claims.html"
-
-
-
-
-
-
-
-/// api-version
-#define kApiVersion             @"api-version"
-/// token 会话时效
-#define k_Http_SessionToken     @"AISessionToken"
-/// 腾讯云appid
-#define kTXYAppId               @"10076082"
-/// TXYUploadManager 实例对应的持久化 id，此 id 必须全局唯一，persistenceId 为 nil 时，上传任务不持久化
-#define kPersistenceId          @"AIHealthPersistenceId"
-/// 腾讯云   目录所属 bucket 名称
-#define kBucket                 @"tongxuebao"
 #define IS_IPAD (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
 #define IS_IPHONE (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
 
